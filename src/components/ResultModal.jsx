125cce0402fb4dbfb6b59f5a61ac826a8c9a8fc1
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-05-25 18:52:52 +0530

    "refs forwarded"

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,11 +1,11 @@
+import { forwardRef } from "react";
 
-
-export default function ResultModal({result, targetTime }) {
+const ResultModal = forwardRef( function ResultModal({result, targetTime },ref) {
 
 
   return (
 
-    <dialog className="result-modal" open>
+    <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -18,4 +18,6 @@ export default function ResultModal({result, targetTime }) {
 
     
   );
-}
+})
+
+export default ResultModal;
