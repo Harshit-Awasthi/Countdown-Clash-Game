9cccaa8d980362a4579a78e650d4c5f844f0c4cd
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-05-25 17:05:22 +0530

    "Added Player Name on Welcome section"

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+
+
+  const playername= useRef();
+
+  const [enteredPlayername, setEnteredPlayername]=useState(null);
+
+  function handleClick(){
+    setEnteredPlayername(playername.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayername ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playername} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
